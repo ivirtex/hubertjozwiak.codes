93cccf50420f03aabbcb37a2db879a725d550684
ivirtex <hjozwiakdx@gmail.com>
2022-01-16 02:23:14 +0100

    Add Splitbee analytics, add mail contact button

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+
 import "../styles/globals.css";
+import splitbee from "@splitbee/web";
 import NextNProgress from "nextjs-progressbar";
 
 import Layout from "../components/layout";
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    splitbee.init();
+  }, []);
+
   return (
     <Layout>
       <NextNProgress height={2} options={{ showSpinner: false }} />
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,21 @@
+import splitbee from "@splitbee/web";
+import copy from "copy-to-clipboard";
 import Head from "next/head";
+import { toast } from "react-hot-toast";
 import { BsDiscord, BsEnvelopeFill } from "react-icons/bs";
+import { IoCopy } from "react-icons/io5";
 
 import Card from "../components/card";
 
 export default function Contact() {
+  let handleCopy = () => {
+    splitbee.track("contact", { type: "mail" });
+    copy("hubertjozwiak@hubertjozwiak.codes");
+    toast.success("Email copied to clipboard!", {
+      style: { background: "#0a0f18", color: "#fff" },
+    });
+  };
+
   return (
     <div className="max-w-2xl m-auto">
       <Head>
@@ -11,16 +23,24 @@ export default function Contact() {
       </Head>
       <div className="text-3xl font-bold">Contact</div>
       <div className="py-4 text-lg">Reach out to me:</div>
-      <a href="https://discordapp.com/users/922104761120084039">
-        <Card className="flex flex-row items-center">
-          <BsDiscord size={25} />
-          <span className="pl-4">Discord</span>
-        </Card>
-      </a>
-      <Card className="flex flex-row items-center">
-        <BsEnvelopeFill size={25} />
-        <span className="pl-4">Email - soon</span>
-      </Card>
+      <address>
+        <a
+          href="https://discordapp.com/users/922104761120084039"
+          onClick={() => splitbee.track("contact", { type: "mail" })}
+        >
+          <Card className="flex flex-row items-center">
+            <BsDiscord size={25} />
+            <span className="pl-4">Discord</span>
+          </Card>
+        </a>
+        <button className="w-full" onClick={handleCopy}>
+          <Card className="flex flex-row items-center">
+            <BsEnvelopeFill size={25} />
+            <span className="pl-4">Email</span>
+            <IoCopy size={25} className="ml-auto" />
+          </Card>
+        </button>
+      </address>
     </div>
   );
 }
