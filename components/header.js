ed4e7b33dc5f6f3efe7369d62a95963e04353777
ivirtex <hjozwiakdx@gmail.com>
2022-05-31 01:15:49 +0200

    Add dropdown menu for mobile

--- components/header.js
+++ components/header.js
@@ -1,19 +1,32 @@
 // @ts-check
+import { useState } from "react";
+
+import { Dialog } from "@headlessui/react";
 import Link from "next/link";
+import { MdMenu, MdClose } from "react-icons/md";
 
 import HeaderButton from "./headerButton";
 import ThemeChanger from "./themeChanger";
 
 export default function Header() {
+  let [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="fixed inset-x-0 top-0 z-50 border-b border-gray-200 bg-opacity-30 backdrop-blur-lg backdrop-filter dark:border-gray-700">
       <div className="mx-auto flex max-w-3xl items-center justify-between space-x-4 p-4">
         <Link href="/" passHref>
-          <button className="h-min text-xl font-semibold hover:text-zinc-600 dark:hover:text-zinc-300 sm:text-2xl">
+          <button className="h-min font-semibold text-2xl hover:text-zinc-600 dark:hover:text-zinc-300">
             ivirtex.dev
           </button>
         </Link>
-        <div className="grid grid-cols-2 gap-2 font-light sm:flex">
+        <button
+          className="rounded-full bg-gray-200 p-3 dark:bg-gray-800 sm:hidden"
+          onClick={() => setIsOpen(true)}
+        >
+          <MdMenu size={25} />
+        </button>
+
+        <div className="hidden font-light sm:flex">
           <Link href="/about" passHref>
             <HeaderButton>About</HeaderButton>
           </Link>
@@ -27,7 +40,50 @@ export default function Header() {
             <HeaderButton>Contact</HeaderButton>
           </Link>
         </div>
-        <ThemeChanger />
+
+        <ThemeChanger className="hidden sm:flex" />
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="fixed inset-0 z-50 md:hidden"
+        >
+          {/* The backdrop, rendered as a fixed sibling to the panel container */}
+          <div
+            className="fixed inset-0 bg-black/30 backdrop-blur-sm"
+            aria-hidden="true"
+            onClick={() => setIsOpen(false)}
+          />
+
+          <div className="fixed top-4 right-4  rounded-lg bg-zinc-100 p-6 font-semibold text-gray-900 shadow-lg dark:bg-gray-900 dark:text-gray-400">
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute top-3 right-3 flex h-8 w-8 items-center justify-center text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
+            >
+              <span className="sr-only ">Close navigation</span>
+              <MdClose size={20} className="fill-red-700" />
+            </button>
+            <ul className="space-y-4">
+              <li>
+                <Link href="/about" passHref>
+                  <HeaderButton>About</HeaderButton>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" passHref>
+                  <HeaderButton>Contact</HeaderButton>
+                </Link>
+              </li>
+              <li>
+                <Link href="/projects" passHref>
+                  <HeaderButton>Projects</HeaderButton>
+                </Link>
+              </li>
+            </ul>
+            <div className="mt-6 border-t border-gray-200 pt-6 dark:border-gray-200/10">
+              <ThemeChanger extended />
+            </div>
+          </div>
+        </Dialog>
       </div>
     </nav>
   );
--- components/themeChanger.js
+++ components/themeChanger.js
@@ -2,8 +2,9 @@
 import { useEffect, useState } from "react";
 
 import { useTheme } from "next-themes";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
-export default function ThemeChanger() {
+export default function ThemeChanger({ className = "", extended = false }) {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -13,100 +14,24 @@ export default function ThemeChanger() {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="flex items-center justify-center rounded-full bg-gray-200 p-3 ring-zinc-800 ring-offset-4 ring-offset-zinc-100 duration-200 ease-in-out hover:ring-2 dark:bg-gray-800 dark:ring-zinc-200 dark:ring-offset-gray-900"
+      className={`flex items-center justify-center rounded-full bg-gray-200 p-3 ring-zinc-800 ring-offset-4 ring-offset-zinc-100 duration-200 ease-in-out hover:ring-2 
+      dark:bg-gray-800 dark:ring-zinc-200 dark:ring-offset-gray-900 ${className}`}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {mounted && (
-        <>
-          <div>
-            {theme === "dark" ? (
-              <svg className="h-7 w-7" fill="none" viewBox="0 0 24 24">
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="3.25"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                ></circle>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M12 2.75V4.25"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M17.25 6.75L16.0659 7.93416"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M21.25 12.0001L19.75 12.0001"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M17.25 17.2501L16.0659 16.066"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M12 19.75V21.25"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M7.9341 16.0659L6.74996 17.25"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M4.25 12.0001L2.75 12.0001"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M7.93405 7.93423L6.74991 6.75003"
-                ></path>
-              </svg>
-            ) : (
-              <svg className="h-7 w-7" fill="none" viewBox="0 0 24 24">
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M18.25 15.7499C17.2352 16.2904 16.23 16.25 15 16.25C10.9959 16.25 7.75 13.0041 7.75 9.00001C7.75 7.77001 7.70951 6.76474 8.25 5.74994C5.96125 6.96891 4.75 9.2259 4.75 12C4.75 16.004 7.99594 19.25 12 19.25C14.7741 19.25 17.031 18.0387 18.25 15.7499Z"
-                ></path>
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M16 4.75C16 6.95914 14.9591 9 12.75 9C14.9591 9 16 11.0409 16 13.25C16 11.0409 17.0409 9 19.25 9C17.0409 9 16 6.95914 16 4.75Z"
-                ></path>
-              </svg>
-            )}
-          </div>
-        </>
+        <div className="font-semibold">
+          {theme === "dark" ? (
+            <div className="flex">
+              <MdOutlineLightMode size={25} />
+              {extended && <span className="mx-2">Light mode</span>}
+            </div>
+          ) : (
+            <div className="flex">
+              <MdOutlineDarkMode size={25} />
+              {extended && <span className="mx-2">Dark mode</span>}
+            </div>
+          )}
+        </div>
       )}
     </button>
   );
